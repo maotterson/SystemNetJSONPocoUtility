4566cf43eea2ac63a641a628b7e5ddf4886b5bde
maotterson <otterson.2@gmail.com>
2022-07-21 00:53:10 -0400

    add unit test for  json data

--- SystemNetJSONPocoUtility.Tests/UnitTests/JsonParserUnitTest.cs
+++ SystemNetJSONPocoUtility.Tests/UnitTests/JsonParserUnitTest.cs
@@ -2,9 +2,22 @@ namespace SystemNetJSONPocoUtility.Tests.UnitTests;
 
 public class JsonParserUnitTest
 {
-    [Fact]
-    public void DeserializeAndFlatten_ShouldReturnPropertyKVP_WhenValidJsonProvided()
-    {
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { json, dict }
+        };
 
+    public static Dictionary<string, string> dict = new Dictionary<string, string>
+    {
+        { "name","john doe" }
+    };
+    const string json = @"{""name"":""john doe""}";
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void DeserializeAndFlatten_ShouldReturnPropertyKVP_WhenValidJsonProvided(string rawJson, Dictionary<string, string> dictionary)
+    {
+        Console.WriteLine(rawJson);
     }
 }
